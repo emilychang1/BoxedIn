a71e6bae6731b011a5296b1ca05901e86b211bba
Emily Chang <emilychang@Emilys-MacBook-Pro-2.local>
2016-12-30 00:51:08 -0800

    enemy added, dino jumps

--- simple.py
+++ simple.py
@@ -21,9 +21,17 @@ class InputLayer(cocos.layer.ColorLayer):
         self.add(self.sprite)
         self.sprite.do(FadeIn(2))
 
-        self.schedule_interval(self.movement, 0.1)
+        self.enemy = Sprite('assets/enemy.png')
+        self.enemy.position = x, y
+        self.enemy.opacity = 0
+        self.add(self.enemy)
+        self.enemy.do(FadeIn(2))
+
+        self.schedule_interval(self.movement, 0.05)
+        self.schedule_interval(self.enemy_attack, 3)
         self.left_move = False
         self.right_move = False
+        self.up_move = False
 
 
     def on_key_press(self, key, modifiers):
@@ -33,14 +41,16 @@ class InputLayer(cocos.layer.ColorLayer):
         elif symbol_string(key) == "RIGHT":
             self.right_move = True
 
-
         elif symbol_string(key) == "SPACE":
-            coordinates = self.sprite.position
+            self.up_move = True
 
-    """ move sprite in the appropriate direction as long as left/right key is held"""
+    
     def movement(self, dt):
 
+        """ move sprite in the appropriate direction as long as left/right key is held"""
+
         move_left = MoveBy((-20, 0), .1)
+        move_up = MoveBy((0, 15), .1) + MoveBy((0, -15), .1)
 
         if self.left_move == True:
             self.sprite.do(move_left)
@@ -48,11 +58,22 @@ class InputLayer(cocos.layer.ColorLayer):
         elif self.right_move == True:
             self.sprite.do(Reverse(move_left)) 
 
+        elif self.up_move == True:
+            self.sprite.do(move_up) 
+
 
     def on_key_release(self, key, modifiers):
 
         self.left_move = False
         self.right_move = False
+        self.up_move = False
+        default_y = MoveBy((0, -(self.sprite.position[1] - 240)), 0.5)
+        self.sprite.do(default_y)
+
+    def enemy_attack(self, dt):
+        distanceFromDino_x = self.sprite.position[0] - self.enemy.position[0]
+        move_toward_dino = MoveBy((distanceFromDino_x, 0), 5)
+        self.enemy.do(move_toward_dino)
 
 
 director.init()
